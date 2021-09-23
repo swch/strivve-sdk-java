f51bbf95bdb38b78c11b5c4bb3f5164eaa4b890d
Mark Budos <markbudos@gmail.com>
2021-09-23 13:14:48 -0700

    make public

--- cardsavr/src/main/java/com/strivve/CardsavrEncryptionException.java
+++ cardsavr/src/main/java/com/strivve/CardsavrEncryptionException.java
@@ -2,7 +2,7 @@ package com.strivve;
 
 import java.io.IOException;
 
-class CardsavrEncryptionException extends IOException {
+public class CardsavrEncryptionException extends IOException {
 
     private static final long serialVersionUID = -3307146660680085641L;
 
